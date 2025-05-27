a4da8eaef65b4fbf911be2cf4d91836f0290e155
artem-yanishev <janyshevtema79@gmail.com>
2025-05-27 19:06:51 +0300

    commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
 
     private String firstName;
@@ -8,7 +10,7 @@ public class Employee {
     private static int nextId = 1;
     private int id;
 
-    public Employee(String firstName, String lastName, String patronymic, int department, int salary, int id) {
+    public Employee(String lastName, String firstName, String patronymic, int department, int salary) {
         this.id = nextId++;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -39,5 +41,39 @@ public class Employee {
 
     public int getId() {
         return id;
+    }
+
+    public void setDepartment(int department) {
+        if(department < 1 || department > 5) {
+            throw new IllegalArgumentException("Отдел должен быть от 1 до 5");
+        }
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Зарплата не может быть отрицательной");
+        }
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(patronymic, employee.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, patronymic, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", ФИО: " + lastName + " " + firstName + " " + patronymic +
+                ", Отдел: " + department +
+                ", Зарплата: " + salary;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
+    private static Employee[] employees = new Employee[10];
+    private static int employeeCount = 0;
+
     public static void main(String[] args) {
-        int[] departmentMass = {1, 2, 3, 4, 5};
+        addEmployee (new Employee("Иванов", "Иван", "Иванович",
+                1, 60000));
+        addEmployee (new Employee("Сергеев", "Сергей", "Сергеевич",
+                2, 70000));
+        printAllEmployees();
+    }
+
+    public static void addEmployee(Employee employee) {
+        if (employeeCount < employees.length) {
+            employees[employeeCount] = employee;
+            employeeCount++;
+        } else {
+            System.out.println("Места нет!");
+        }
+    }
+
+    public static void printAllEmployees() {
+        System.out.println("=== Список сотрудников ===");
+        for (int i = 0; i < employeeCount; i++) {
+            System.out.println(employees[i].toString());
+        }
+        System.out.println("==========================");
     }
 }
